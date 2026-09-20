f4e0f2f3d3a7d39086b1bcabb82fd8bbd04ba0f5
OmkarBaviskar <omkar.baviskar@xpressbees.com>
2026-09-20 13:42:00 +0530

    insertion sort

--- C++/code/sorting/insertionSort.cpp
+++ C++/code/sorting/insertionSort.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 void insertionSort(vector<int>& v, int size) {
-    
+    for(int i=0; i<size-1; i++) {
+        if(v[i]>v[i+1]) {
+            int temp = v[i+1];
+            v[i+1] = v[i];
+            v[i] = temp;
+        }
+        for(int j=i; j>0; j--) {
+            if(v[j]<v[j-1]) {
+                int temp = v[j];
+                v[j] = v[j-1];
+                v[j-1] = temp;
+            } else break;
+        }
+    }
 }
 
 int main() {
